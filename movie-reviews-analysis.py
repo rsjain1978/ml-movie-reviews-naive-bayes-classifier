816e35f2748f0ab5e027ed48fb735d087b533545
rsjain1978 <rsjain1978@gmail.com>
2019-02-23 02:12:18 +0530

    cleaned code

--- movie-reviews-analysis.py
+++ movie-reviews-analysis.py
@@ -8,61 +8,32 @@ df = pd.read_csv('movie-pang02-train.csv')
 import re
 from nltk.corpus import stopwords
 
-def preProcessData(data):
-    countMessages = data['reviews'].size
-
-    processedReviews = []
-
-    for i in range(0, countMessages):
-        review = data['reviews'][i]
-        letters_only = re.sub("[^a-zA-Z]",' ', review)
-        words = letters_only.lower().split()
-        stops = set(stopwords.words("english"))
-        filtered_words = [w for w in words if not w in stops]
-        processedReviews.append(" ".join(filtered_words))
-
-    return processedReviews
-
 print ('Pre-processing')
-processedReview = preProcessData(df)
-
-df["p_reviews"] = processedReview
-columns = ['p_reviews','category']
-df=df[columns]
-
-from sklearn.preprocessing import LabelEncoder
-lc = LabelEncoder()
-df['category'] = lc.fit_transform(df['category'])
 
 #convert text into numbers
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
-cv = CountVectorizer(analyzer='word')
-train_reviews = cv.fit_transform(df['p_reviews'])
-train_category = df['category']
+from sklearn.pipeline import Pipeline
 
-tfidf_transformer = TfidfTransformer()
-train_reviews_tfidf = tfidf_transformer.fit_transform(train_reviews)
+pipeline = Pipeline([
+    ('vect', CountVectorizer(stop_words='english', lowercase=True)),
+    ('tfidf', TfidfTransformer(use_idf=True, smooth_idf=True)),
+    ('clf', MultinomialNB(alpha=1))
+
+])
 
 print ('Training')
-
-mb = MultinomialNB(fit_prior=True)
-clf = mb.fit(train_reviews_tfidf, train_category)
-
-testDF = pd.read_csv('movie-pang02-test.csv')
-p_test_reviews = preProcessData(testDF)
-testDF = pd.DataFrame(data=p_test_reviews, columns=['reviews'])
-test_reviews_cv = cv.transform(testDF['reviews'])
+pipeline.fit(df['reviews'], df['category'])
 
 print ('Predicting')
-
-predictedY = clf.predict(test_reviews_cv)
+testDF = pd.read_csv('movie-pang02-test.csv')
+predictedY = pipeline.predict(testDF['reviews'])
 
 for i in range(0, predictedY.size):
-    if (predictedY[i]==0):
+    if (predictedY[i]=='Neg'):
         print ('Bad Review')
-    if (predictedY[i]==1):
+    if (predictedY[i]=='Pos'):
         print ('Good Review')
 
